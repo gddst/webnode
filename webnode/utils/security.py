51d6e4212678838aef1728dd1243b5657ae1b7f2
rz <rz@aecworks.com>
2018-01-07 23:35:33 +0800

    Fixed str related error

--- webnode/utils/security.py
+++ webnode/utils/security.py
@@ -1,27 +1,25 @@
-import base64
 import binascii
 import hashlib
 import os
-import pprint
 
 
 class SecurityUtil(object):
-    
     @staticmethod
     def Hash_pw(password):
         return hashlib.sha256(password).hexdigest()
-    
+
     @staticmethod
     def salted_hash_pw(password):
-        salt=binascii.hexlify(os.urandom(32))
-        salted_pw=hashlib.sha256(salt+password).hexdigest()
-        return salt+':'+salted_pw
-    
+        password = bytes(password, encoding='utf-8')
+        salt = binascii.hexlify(os.urandom(32))
+        salted_pw = hashlib.sha256(salt + password).hexdigest()
+        salted_pw = bytes(salted_pw, encoding='utf-8')
+        return salt + b':' + salted_pw
+
     @staticmethod
     def validate_pw(salted_has_pw, password):
-        salt=salted_has_pw.split(':')[0]
-        pw=salted_has_pw.split(':')[1]
-        return hashlib.sha256(salt+password).hexdigest()==pw
-        
-if __name__=="__main__":
-    pass
+        password = bytes(password, encoding='utf-8')
+        salt = salted_has_pw.split(b':')[0]
+        saved_hash = salted_has_pw.split(b':')[1]
+        input_hash = bytes(hashlib.sha256(salt + password).hexdigest(), encoding='utf-8')
+        return input_hash == saved_hash
